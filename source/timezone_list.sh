3f49ce294628d9b20b3f8fe8334b7634ec730555
Ragnar Nevries <ragnar.nevries@camunda.com>
2019-03-17 19:16:45 -0400

    all features and fixes of v2.5

--- source/timezone_list.sh
+++ source/timezone_list.sh
@@ -2,125 +2,207 @@ source includes.sh
 
 search="$1"	#Alfred argument
 
-# Check if argument could be a time in one of the formats:
-# HH
+# Argument can be empty or have 1, 2 or 3 space separated components
+# empty: use current time on current date at local timezone
+# one component: <time> or <city> - use given time on current date at local timezone or sarch for city
+# two components: <date modification> <time> - use given time on calculated date at local timezone
+# three components: <source timezone> <date modification> <time> - use given time on calculated date at given source timezone
+
+# <time> can have following formats:
+# HH (assume minutes to be zero)
 # HH:MM
 # HHMM
 
-re='^[0-9]+'
-if [[ $search =~ $re ]] ; then
+# <date modification> can have following formats:
+# t (short for today)
+# today
+# tm (short for tomorrow)
+# tomorrow
+# DD (assume current month and year)
+# MMDD (assume current year)
+# YYMMDD
+# DDd (DD days added to today)
+
+# <source timezone> can have following formats:
+# CITY (to be looked up in /usr/share/zoneinfo.default)
+
+comp1=$(echo "$search" | awk -F'[[:space:]]+' '{print $1}')
+comp2=$(echo "$search" | awk -F'[[:space:]]+' '{print $2}')
+comp3=$(echo "$search" | awk -F'[[:space:]]+' '{print $3}')
+
+if [ -n "$comp3" ]
+then
+    source_timezone_search=$comp1
+    date_modification_search=$comp2
+    time_search=$comp3
+elif [ -n "$comp2" ]
+then
+    date_modification_search=$comp1
+    time_search=$comp2
+else
+    if [[ "$comp1" =~ ^[0-9]+ ]]
+    then
+        time_search=$comp1
+    else
+        city_search=$comp1
+    fi
+fi
+
+#
+# create source timezone
+#
+if [ -n "$source_timezone_search" ]
+then
+    #timezoneToConvert=$(find /usr/share/zoneinfo.default -iname "*${source_timezone_search}*" | head -1 )
+    #timezoneToConvert=${timezoneToConvert:28}
+    timezoneToConvert=$(cat "$timezone_file" | awk -v query="$source_timezone_search" -F'|' 'tolower($1) ~ tolower(query) {print $3}' )
+    if [ -n "$timezoneToConvert" ]
+    then
+        timezoneOffsetToConvert=$(TZ=$timezoneToConvert date +%z)
+    fi
+fi
+if [ -z "$timezoneOffsetToConvert" ]
+then
+    timezoneOffsetToConvert=$(date +%z)
+fi
+
+#
+# create source date
+#
+if [ 'tm' = "$date_modification_search" -o 'tomorrow' = "$date_modification_search" ]
+then
+    dateToConvert=$(date -v +1d +%Y%m%d)
+elif [[ "$date_modification_search" =~ ^[0-9]+d$ ]]
+then
+    dateToConvert=$(date -v +${date_modification_search} +%Y%m%d)
+elif [[ "$date_modification_search" =~ ^[0-9]{1,2}$ ]]
+then
+    dateToConvert=$(date -v ${date_modification_search}d +%Y%m%d)
+elif [[ "$date_modification_search" =~ ^[0-9]{3}$ ]]
+then
+    dateToConvert=$(date -v ${date_modification_search:0:1}m -v ${date_modification_search:1}d +%Y%m%d)
+elif [[ "$date_modification_search" =~ ^[0-9]{4}$ ]]
+then
+    dateToConvert=$(date -v ${date_modification_search:0:2}m -v ${date_modification_search:2}d +%Y%m%d)
+elif [[ "$date_modification_search" =~ ^[0-9]{5}$ ]]
+then
+    dateToConvert=$(date -v 20${date_modification_search:0:1}y -v ${date_modification_search:1:2}m -v ${date_modification_search:3}d +%Y%m%d)
+elif [[ "$date_modification_search" =~ ^[0-9]{6}$ ]]
+then
+    dateToConvert=$(date -v 20${date_modification_search:0:2}y -v ${date_modification_search:2:2}m -v ${date_modification_search:4}d +%Y%m%d)
+elif [[ "$date_modification_search" =~ ^[0-9]{8}$ ]]
+then
+    dateToConvert=$(date -v ${date_modification_search:0:4}y -v ${date_modification_search:4:2}m -v ${date_modification_search:6}d +%Y%m%d)
+else
+    # fallback that also covers 't' and 'today'
+    dateToConvert=$(date +%Y%m%d)
+fi
+
+#
+# create source time
+#
+if [[ $time_search =~ ^[0-9:]+$ ]] ; then
 
     # HH
-    if [[ $search =~ ^[0-9]{2}$ ]]; then
-        echo "2 digits" >> /tmp/alfred.txt
-        search=$search"00"
+    if [[ $time_search =~ ^[0-9]{2}$ ]]; then
+        #echo "2 digits" >> /tmp/alfred.txt
+        time_search=${time_search}00
     fi
     
     # H (pad to 0H)
-    if [[ $search =~ ^[0-9]{1}$ ]]; then
-        echo "1 digit" >> /tmp/alfred.txt
-        search="0"$search"00"
+    if [[ $time_search =~ ^[0-9]{1}$ ]]; then
+        #echo "1 digit" >> /tmp/alfred.txt
+        time_search=0${time_search}00
     fi
     
     # H: (pad to 0H:)
-    if [[ $search =~ ^[0-9]{1}\: ]]; then
-        echo "1 digit" >> /tmp/alfred.txt
-        search="0"$search
+    if [[ $time_search =~ ^[0-9]{1}\: ]]; then
+        #echo "1 digit" >> /tmp/alfred.txt
+        time_search=0${time_search}
     fi    
     
-    search="${search//:}"
+    # pad seconds
+    time_search=${time_search//:}00
 
-    echo ${search} >> /tmp/alfred.txt
-    timeToConvert=${search}
-    search=""
+    #echo ${time_search} >> /tmp/alfred.txt
+    timeToConvert=${time_search}
+
+    # todo this is weird
+    time_search=
+else
+    timeToConvert=$(date +%H%M)00
+    time_search=
 fi
 
 #Populate Alfred results with Timezones list
 echo '<?xml version="1.0"?>
-	<items>'
+    <items>'
 
 match=0		#use to determine if there are any matches to the current query in Alfred
 
 
 if [[ "$TIME_FORMAT" = "24h" ]]; then
-    TIME_FORMAT_STR='%k:%M'
+    TIME_FORMAT_STR='%0k:%M'
 
 else
-    TIME_FORMAT_STR='%l:%M %p'
+    TIME_FORMAT_STR='%-l:%M %p'
 fi
 
-while IFS= read -r line
-	do
+while IFS='|' read -r city country timezone country_code telephone_code favourite
+    do
 
-	if [[ "$line" == \#* ]]
-	then
-		continue
-	fi
+    # skip comment line
+    if [[ "$city" =~ ^[[:space:]]*\# ]]
+    then
+        continue
+    fi
 
-	# 	echo "$city|$country|$timezone|$country_code"
+    if [ -n "$telephone_code" ]
+    then
+        telephone_code_string=" (+$telephone_code)"
+    fi
 
-	OIFS=$IFS
-	IFS='|'		#Split stored line by delimiter
-	data=($line)	#Create array
-	city="${data[0]}"
-	country="${data[1]}"
-	timezone="${data[2]}"
-	country_abbr="${data[3]}"
-	country_code="${data[4]}"
-	favourite="${data[5]}"
-	if [ -n "$country_code" ]
-	then
-	    country_code_string=" (+$country_code)"
-	fi
+    if [[ "$favourite" == "0" ]]
+    then
+        favourite_string="⭐️ •"
+    else
+        favourite_string=""	
+    fi
+    
+    if [ "$timezone" = "$timezoneToConvert" ]
+    then
+        sourceTimezone_string="👉 "
+    else
+        sourceTimezone_string=""
+    fi
+    
+    setTimeOptionArguments="-jf %Y%m%d%H%M%S%z $dateToConvert$timeToConvert$timezoneOffsetToConvert"
 
-	if [[ "$favourite" == "0" ]]
-	then
-	    favourite_string="⭐️ •"
-	else
-	    favourite_string=""	
-	fi
+    city_time=$(TZ=$timezone date $setTimeOptionArguments +"$TIME_FORMAT_STR")
+    city_date=$(TZ=$timezone date $setTimeOptionArguments +"%A, %d %B %Y" )
 
-	
-	IFS=$OIFS
+    #Determine flag icon
+    country_flag=$(echo "$country" | tr '[A-Z]' '[a-z]')
+    country_flag=${country_flag// /_}
+    flag_icon=$country_flag.png
+    if [ ! -e ./flags/$flag_icon ]; then
+        flag_icon="_no_flag.png"
+    fi
 
+    # we start the output with a sort key to simply pipe the result to 'sort'
+    # we sort first by favourite, second by time ascending, third by city name
+    sortkey=$favourite$(TZ=$timezone date $setTimeOptionArguments +%Y%m%d%H%M )"$city"
 
-	# if [ $offset -ge 0 ]; then		#Add or subtract offset from UTC (in Unix epoch seconds)
-	# 	city_epochtime=$(date -j -v "+$offset"S +%s ${timeToConvert})	
-	# else
-	# 	city_epochtime=$(date -j -v "$offset"S +%s ${timeToConvert}) 
-	# fi
-	# city_time=$(date -u -j -f %s $city_epochtime +"$TIME_FORMAT_STR") #Create readable time expression
-	# city_date=$(date -u -j -f %s $city_epochtime +"%A %e %B %Y") #Create readable date expression
-
-	local_tz=$(date +%z)
-
-	if [[ ! -z "$timeToConvert" ]]
-	then
-		time_to_convert="-jf %H%M%z $timeToConvert$local_tz"
-	else
-		time_to_convert=''
-	fi
-
-	city_time=$(TZ=$timezone date $time_to_convert +"$TIME_FORMAT_STR")
-	city_date=$(TZ=$timezone date $time_to_convert +"%A, %d %B" )
-
-	#Determine flag icon
-	country_flag=$(echo "$country" | tr '[A-Z]' '[a-z]')
-	country_flag=${country_flag// /_}
-	flag_icon=$country_flag.png
-	if [ ! -e ./flags/$flag_icon ]; then
-		flag_icon="_no_flag.png"
-	fi
-	#echo "$flag_icon" >> ~/Desktop/flags.txt
-	if [[ "$city" == "$search"* ]]; then
-		match=1
-		echo '<item arg="'$city, $city_time'" valid="yes">
-		<title>'$city: $city_time'</title>	
-		<subtitle>'$favourite_string' on '$city_date' • '${country}${country_code_string}' • Timezone: '$timezone'</subtitle>
-		<icon>./flags/'$flag_icon'</icon>
-		</item>'
-	fi
-done < <(sort -k 6 -k 1 -t "|" "$timezone_file")
+    if [[ "$city" =~ ${city_search:-.} ]]; then
+        match=1
+        echo "<!--$sortkey-->\
+              <item arg=\"$city, $city_time\" valid=\"yes\">\
+                  <title>$sourceTimezone_string$city: $city_time</title>\
+                  <subtitle>$favourite_string on $city_date • ${country}${telephone_code_string} • Timezone: $timezone</subtitle>\
+                  <icon>./flags/$flag_icon</icon>\
+              </item>"
+    fi
+done < "$timezone_file" | sort
 
 echo '</items>'
 
